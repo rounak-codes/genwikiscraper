51cd30850400bc6cf4fbbb56db47b8e70aabd893
Rounak Kumar Bag <rounakbag24@gmail.com>
2024-11-11 15:14:55 +0530

    First upload

---
+++ genwikiartifacts.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 15:02:52 2024
+
+@author: rounak-codes
+"""
+
+from urllib import request
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://genshin-impact.fandom.com/wiki/Artifact/Sets"
+resp = request.urlopen(url)
+data = resp.read()
+soup = BeautifulSoup(data, 'html.parser')
+table = soup.find("table")
+trs = table.find_all("tr")
+name, quality, bonuses = [], [], []
+
+for tr in trs[0:]: # Skip the header row
+    tds = tr.find_all("td")
+    if tds:
+        name.append(tds[0].text.strip())
+        quality.append(tds[1].text.strip())
+        bonuses.append(tds[3].text.strip() if len(tds) > 3 else '')
+
+df = pd.DataFrame({"name": name, "quality": quality, "bonuses": bonuses})
+print(df)
+
+df.to_csv('genwikiartifacts.csv')
---
+++ genwikiweapons.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 14:48:16 2024
+
+@author: rounak-codes
+"""
+
+from urllib import request
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://genshin-impact.fandom.com/wiki/Weapon/List"
+resp = request.urlopen(url)
+data = resp.read()
+soup = BeautifulSoup(data, 'html.parser')
+table = soup.find("table")
+trs = table.find_all("tr")
+name, quality, base_atk, substat, passive = [], [], [], [], []
+
+for tr in trs[1:]: # Skip the header row
+    tds = tr.find_all("td")
+    if tds:
+        name.append(tds[1].text.strip())
+        quality.append(tds[2].text.strip())
+        base_atk.append(tds[3].text.strip())
+        substat.append(tds[4].text.strip())
+        passive.append(tds[5].text.strip() if len(tds) > 5 else '')
+
+df = pd.DataFrame({"name": name, "quality": quality, "base_atk": base_atk, 
+                   "substat": substat, "passive": passive})
+print(df)
+
+df.to_csv('genwikiweapons.csv')
